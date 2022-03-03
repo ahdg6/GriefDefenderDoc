d8dfaa322d9e17cf8a766be1152fece52e25fc15
ahdg <ahdg6@outlook.com>
2022-03-03 16:38:37 +0800

    整理

--- docs/zh/_sidebar-zh.ts
+++ docs/zh/_sidebar-zh.ts
@@ -12,18 +12,24 @@ export const zh = defineSidebarConfig({
 		  collapsable: false,
           children: ["", "install", "commands", "permissions"],
         },
+        {
+          text: "领地",
+          icon: "fa-solid fa-house-heart",
+          prefix: "/zh/guide/claim/",
+          collapsable: false,
+          children: ["claim-types", "flag-definitions-gui", "flags", "trust-system"],
+        },
 		{
 		  text: "基础",
 		  icon: "fa-solid fa-laptop-code",
 		  prefix: "/zh/guide/basic/",
-		  children: ["claim-management", "claim-types", "custom-alias-creation",
-		  "flag-definitions-gui", "options", "pvp", "tags", "trust-system", "claim-rent-system"],
+		  children: ["claim-management", "options", "pvp", "tags", "claim-rent-system"],
 		},
 		{
 		  text: "进阶",
 		  icon: "fa-solid fa-grid",
 		  prefix: "/zh/guide/advanced/",
-		  children: ["contexts", "debugging", "flags", "global-config"],
+		  children: ["contexts", "debugging", "global-config"],
 		},
 		{
 		  text: "迁移",
